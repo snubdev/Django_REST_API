2ac4f04e928847f22aa7995f5a689bdcb577a5a4
Maicon Leandro <snubhousedev@gmail.com>
2023-03-23 01:06:46 -0300

    Add

---
+++ Estudo Django Api (Alura) 4/escola/views.py
@@ -0,0 +1,64 @@
+from rest_framework import viewsets, generics, status
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunosSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculaAlunoSerializer, ListaAlunosMatriculadosSerializer, AlunosSerializerV2
+from rest_framework.response import Response
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    # serializer_class = AlunosSerializer
+
+    def get_serializer_class(self):
+        if self.request.version == 'v2':
+            return AlunosSerializerV2
+        else:
+            return AlunosSerializer
+
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+            id = str(serializer.data['id'])
+            response['Location'] = request.build_absolute_uri() + id
+            return response
+
+
+class MatriculasViewSet(viewsets.ModelViewSet):
+    """Exibindo todas as Matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+    # Restringir o DELETE NA API
+    http_method_names = ['get', 'post', 'put', 'path']
+
+    @method_decorator(cache_page(20))
+    def dispatch(self, *args, **kwargs):
+        return super(MatriculasViewSet, self).dispatch(*args, **kwargs)
+
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as matrícula de uma aluno ou aluna"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculaAlunoSerializer
+
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listando alunos e alunas matriculados em um curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Django REST APIs alura
+
+## Cursos:
+
+### API com Django 3: Django Rest Framework
+### API com Django 3: Validações, buscas, filtros e deploy
+### API com Django 3: Versionamento, cabeçalhos e CORS
+### API com Django 3: Aprofundando em testes e documentação
+
+:white_check_mark:
